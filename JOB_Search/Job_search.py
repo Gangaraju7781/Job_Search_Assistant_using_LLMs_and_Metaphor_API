5c2e4f5c07485f37a755481788d0f45aacf10030
Bhaskar Abbireddy <61122453+Gangaraju7781@users.noreply.github.com>
2023-10-02 13:31:20 -0400

    Made changes to image url

--- JOB_Search/Job_search.py
+++ JOB_Search/Job_search.py
@@ -9,15 +9,15 @@ openai.api_key = st.sidebar.text_input("API-KEY",type="password")
 # Metaphor API key
 metaphor = Metaphor(st.sidebar.text_input("METAPHOR-KEY",type="password"))
 
-image_url = 'https://assets.website-files.com/60afacf3bc3bf37dd0ad97c5/617ab3cd027d61b4591da3f9_job-search.jpg'
+# image_url = 'https://assets.website-files.com/60afacf3bc3bf37dd0ad97c5/617ab3cd027d61b4591da3f9_job-search.jpg'
 
-st.markdown(f"<div style='display: flex; justify-content: center;'><img src='{image_url}' alt='Job Image' width='200' /></div>", unsafe_allow_html=True)
-
-st.write("") 
-st.write("") 
+# st.markdown(f"<div style='display: flex; justify-content: center;'><img src='{image_url}' alt='Job Image' width='200' /></div>", unsafe_allow_html=True) 
 
 # App title
 st.markdown("<h2 style='text-align: center;'> Job Search Assistant 🔍 </h2><p style='text-align: center; font-size: 18px;'>Goodbye to Experience-Level Uncertainity with LLMs and Metaphor API 👋</p>", unsafe_allow_html=True)
+
+st.write("") 
+st.write("")
 
 # User input for the question
 USER_QUESTION = st.text_input("Enter the job title:")
